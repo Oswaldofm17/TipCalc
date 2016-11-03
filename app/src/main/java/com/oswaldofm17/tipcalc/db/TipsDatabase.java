218b9bfa0178d8a99aa066a8059e358e8045d289
Oswaldo <ozwaldofm.17@gmail.com>
2016-11-03 08:53:53 -0600

    Add ORM Database Specification

---
+++ app/src/main/java/com/oswaldofm17/tipcalc/db/TipsDatabase.java
@@ -0,0 +1,17 @@
+package com.oswaldofm17.tipcalc.db;
+
+import com.raizlabs.android.dbflow.annotation.Database;
+
+/**
+ * Created by oswaldofm on 11/3/16.
+ */
+
+@Database(name = TipsDatabase.NAME, version = TipsDatabase.VERSION)
+
+public class TipsDatabase {
+
+    public static final String NAME = "Tips";
+    public static final int VERSION = 1;
+
+
+}
